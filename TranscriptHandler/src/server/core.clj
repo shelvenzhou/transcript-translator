5938fb66a4d3860c1f265bf16fbdc303f8c6fa71
Shelven Zhou <favoring.zhou49@gmail.com>
2018-04-17 23:44:49 +0800

    add session to limit memory consumption

--- TranscriptHandler/src/server/core.clj
+++ TranscriptHandler/src/server/core.clj
@@ -1,37 +1,36 @@
 (ns server.core
   (:require [server.sentences   :refer [strings->sentences]]
             [server.charset     :refer [wrap-charset]]
+            [server.translate   :refer [translate]]
+            [server.session     :refer [new-session get-session]]
             [compojure.core     :refer :all]
             [compojure.handler  :refer [api]]
             [ring.util.response :refer [charset response]]
-            [ring.adapter.jetty :refer [run-jetty]]
-            [clj-http.client    :as client]))
+            [ring.adapter.jetty :refer [run-jetty]]))
 
-; this serves as a data queue
-(def snippets (repeatedly promise))
+(defn create-session []
+  (let [snippets (repeatedly promise)
+        translations (delay (map translate
+                                 (strings->sentences (map deref snippets))))]
+    (new-session {:snippets snippets :translations translations})))
 
-(def translator "http://localhost:3001/translate")
+(defn accept-snippet [session n text]
+  (deliver (nth (:snippets session) n) text))
 
-(defn translate [text]
-  (future
-    (:body (client/post translator {:body text}))))
-
-(def translations
-  (delay
-    (map translate (strings->sentences (map deref snippets)))))
-
-(defn accept-snippet [n text]
-  (deliver (nth snippets n) text))
-
-(defn get-translation [n] 
-  @(nth @translations n))
+(defn get-translation [session n]
+  @(nth @(:translations session) n))
 
 (defroutes app-routes
-  (PUT "/snippet/:n" [n :as {:keys [body]}]
-    (accept-snippet (Integer. n) (slurp body))
-    (response "OK"))
-  (GET "/translation/:n" [n] 
-    (response (get-translation (Integer. n)))))
+  (POST "/session/create" []
+    (response (str (create-session))))
+  (context "/session/:session-id" [session-id]
+    (let [session (get-session (Integer. session-id))]
+      (routes
+        (PUT "/snippet/:n" [n :as {:keys [body]}]
+          (accept-snippet session (Integer. n) (slurp body))
+          (response "OK"))
+        (GET "/translation/:n" [n]
+          (response (get-translation session (Integer. n))))))))
 
 (defn -main [& args]
   (run-jetty (wrap-charset (api app-routes)) {:port 3000}))
--- TranscriptTest/src/test/core.clj
+++ TranscriptTest/src/test/core.clj
@@ -1,13 +1,32 @@
 (ns test.core
   (:require [test.jabberwocky :refer :all]
+            [test.potato      :refer :all]
             [clj-http.client  :as client]))
 
-; a thread to fetch the translation results
-(future
-  (doseq [n (iterate inc 0)]
-    (println (:body (client/get (str "http://localhost:3000/translation/" n))))))
+(def endpoint "http://localhost:3000")
 
-; main thread to put snippets to handler
+(defn mk-url
+  ([path]
+    (str endpoint path))
+  ([session-id path]
+    (mk-url (str "/session/" session-id path))))
+
+(defn deliver-transcript [transcript]
+  (let [session-id (:body (client/post (mk-url "/session/create")))]
+    (doseq [n (range (count transcript))]
+      (client/put (mk-url session-id (str "/snippet/" n)) {:body (nth transcript n)}))
+    session-id))
+
+(defn print-translation [session-id]
+  (doseq [n (iterate inc 0)]
+    (println (:body (client/get (mk-url session-id (str "/translation/" n)))))))
+
+(def jabberwocky-session (future (deliver-transcript jabberwocky)))
+(def potato-session (future (deliver-transcript potato)))
+
+; two threads to simulate two clients
+(future (print-translation @jabberwocky-session))
+(future (print-translation @potato-session))
+
 (defn -main [& args]
-  (doseq [n (range (count jabberwocky))]
-    (client/put (str "http://localhost:3000/snippet/" n) {:body (nth jabberwocky n)})))
+  nil)
